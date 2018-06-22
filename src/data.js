ca1be6bc7d3d5e5230b46baefcca3588a31c80a3
MaricruzJ <maricruzj.er@gmail.com>
2018-06-22 15:39:11 -0500

    Actualización DEMO 2

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+// data.js para todas las funciones que vimos que obtienen y manipulan los datos
+// 4 funciones
 const getJSONcohorts = (url, callback) => {
     const xhr = new XMLHttpRequest();
     xhr.onload = _ => {
@@ -34,6 +36,7 @@ const getJSONcohorts = (url, callback) => {
     xhr.send();   
   };
 
+  
   const getJSONprogress = (url, callback) => {
     const xhr = new XMLHttpRequest();
     xhr.onload = _ => {
@@ -51,48 +54,3 @@ const getJSONcohorts = (url, callback) => {
     xhr.open('GET', url);
     xhr.send();   
   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-const getCohorts = (callback) => { 
-    const req = new XMLHttpRequest();
-    req.open('GET', '../data/cohorts.json', true);
-    req.onreadystatechange = function (aEvt) {
-        if (req.readyState == 4) {
-            if (req.status == 200) {
-                let dataCohorts = JSON.parse(req.responseText);
-                callback(null, dataCohorts);
-            }
-            else {
-                callback('Error', null);
-            }
-        }
-    };
-    req.send(null);
-}
-
-getCohorts((err, result) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(result);
-        //document.getElementById("txtStudent").value=result;
-
-    }
-})
-
- */
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// usar main.js para todo tu código que tenga que ver con mostrar los datos en la pantalla
+// DOM
+
+// Info necesaria: controlador-> main.js (aqui va el fetch o xhr), modelo-> data.js, vista-> 
 getJSONcohorts('../data/cohorts.json', (err, jsonCohorts) => {
   const selectElementG = document.getElementById('cohortGeneral');
 
@@ -16,6 +20,7 @@ getJSONcohorts('../data/cohorts.json', (err, jsonCohorts) => {
     optionsElements.appendChild(contenidoOptions);
     selectElementG.appendChild(optionsElements);
   }
+
   const selectElementD = document.getElementById('cohortDetallado');
   for (let i = 0; i < cohortGeneral.length; i++) {
     const optionsElements = document.createElement('option');
@@ -25,7 +30,7 @@ getJSONcohorts('../data/cohorts.json', (err, jsonCohorts) => {
   }
 
 });
-
+let x = [];
 getJSONusers('../data/cohorts/lim-2018-03-pre-core-pw/users.json', (err, jsonUsers) => {
 
   if (err) {
@@ -33,13 +38,18 @@ getJSONusers('../data/cohorts/lim-2018-03-pre-core-pw/users.json', (err, jsonUse
     return console.error(err);
   }
 
-  const cohortUsers = jsonUsers;
-  console.log(cohortUsers);
-  console.log(cohortUsers.length);
+  const users = jsonUsers;
+  console.log(users);
 
-  for (let i = 0; i < cohortUsers.length; i++) {
-    //console.log(cohortUsers[i].id);
+  for (let i = 0; i < users.length; i++) {
+    //console.log(users[i].id);
+    x[i] = users[i].id;
   }
+  console.log(x);
+  console.log(users);
+  return users;
+
+
 });
 
 getJSONprogress('../data/cohorts/lim-2018-03-pre-core-pw/progress.json', (err, jsonProgress) => {
@@ -47,9 +57,48 @@ getJSONprogress('../data/cohorts/lim-2018-03-pre-core-pw/progress.json', (err, j
     // algo salió mal...
     return console.error(err);
   }
-
-  console.log(jsonProgress)
-  for (let i = 0; i < jsonProgress.length; i++) {
-    console.log(jsonProgress[i]);
-  }
+  console.log(jsonProgress);
+  let obj = {
+    total: 0,
+    completado: 0,
+    porcentaje: 0
+  };
+  let c = 0;
+  
+ 
+  
+    for (let i = 0; i < x.length; i++) {
+    // Obtengo el objeto que corresponde a cada id de usuario
+    //let y = jsonProgress[x[i]];
+    //console.log(y);
+    // Crear una función con el argumento (users: arreglo de objetos obtenido de la data en bruto) 
+    //y su *valor de retorno* sea un objeto exercises con 3 propiedades: total, completed, percent.
+    if (jsonProgress[x[i]].intro == undefined) {
+      console.log('no hay datos para este alumno');
+      c++;
+    } else {
+      // ejercicios guiados
+      let w = jsonProgress[x[i]].intro.units['02-variables-and-data-types'].parts['04-guided-exercises'].completed;
+      //console.log(w);
+      // Ejercicios no guiados
+      let z = jsonProgress[x[i]].intro.units['02-variables-and-data-types'].parts['06-exercises'].completed;
+      //console.log(z);
+      let xyz = ((w + z) / 2) * 100;
+      obj = {
+        id: x[i],
+        total: 0,
+        completado: jsonProgress[x[i]].intro.units['02-variables-and-data-types'].parts['04-guided-exercises'].completed,
+        porcentaje: xyz
+      };
+      /* for (let j = 0; j < 4; j++) {
+        const trElements = document.createElement('tr');
+        const tdElements = document.createElement('td');
+        const contenidoTh = document.createTextNode(x[i].id);
+        tdElements.appendChild(contenidoTh);
+        tablaElement.appendChild(tdElements);
+      } */
+      console.log(obj);
+    }
+  };
+  console.log(c);
 });
